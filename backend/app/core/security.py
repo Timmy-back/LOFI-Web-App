f801c65799d3c6f119d70cd7a8881e246a188d1e
Timmy-DE <timkhorishikh98@gmail.com>
2026-09-09 15:19:26 +0800

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,4 +1,7 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +12,9 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password:str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) +  timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
