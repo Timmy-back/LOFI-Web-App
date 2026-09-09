537eb0577d631809e6716d590d622013d33c6fb8
Timmy-DE <timkhorishikh98@gmail.com>
2026-09-09 14:42:25 +0800

    Update user.py

--- backend/app/services/user.py
+++ backend/app/services/user.py
@@ -8,7 +8,6 @@ from app.models.user import User
 class EmailAlreadyRegisteredError(Exception):
     pass
 
-
 async def create_user(*, db: AsyncSession, user_in: UserCreate) -> User:
     repo = UserRepository(db=db)
     user = await repo.create(
