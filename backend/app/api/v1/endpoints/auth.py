33e9fced5f7f0e05f652ee29088d77f0ee0df356
Timmy-DE <timkhorishikh98@gmail.com>
2026-09-11 13:24:17 +0800

    Update auth.py

--- backend/app/api/v1/endpoints/auth.py
+++ backend/app/api/v1/endpoints/auth.py
@@ -4,6 +4,9 @@ from app.schemas.user import UserCreate, UserOut, Token
 from app.services.user import authenticate_user, create_user, EmailAlreadyRegisteredError
 from app.repositories.user import UserRepository, get_user_repository
 from app.core.security import create_access_token
+from app.dependencies import get_current_user
+from app.models.user import User
+
 
 
 router = APIRouter()
@@ -16,15 +19,14 @@ async def register(user_in: UserCreate, repo: UserRepository = Depends(get_user_
     except EmailAlreadyRegisteredError:
         raise HTTPException(status_code=400, detail="Email already registered")
 
-
 @router.post("/login", response_model=Token)
-async def login(
-    user_in: UserCreate,
-    repo: UserRepository = Depends(get_user_repository),
-):
+async def login(user_in: UserCreate, repo: UserRepository = Depends(get_user_repository),):
     user = await authenticate_user(repo=repo, email=user_in.email, password=user_in.password)
     if user is None:
         raise HTTPException(status_code=401, detail="Incorrect email or password")
-
     access_token = create_access_token({"sub": str(user.id)})
     return {"access_token": access_token, "token_type": "bearer"}
+
+@router.get("/me", response_model=UserOut)
+async def me(curret_user: User = Depends(get_current_user)):
+    return curret_user
