3c1f6e83b95d4f698ea972c8cc222a602072fff2
Timmy-DE <timkhorishikh98@gmail.com>
2026-09-11 13:24:21 +0800

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -9,8 +9,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-
-def verify_password(plain_password:str, hashed_password: str) -> bool:
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict) -> str:
@@ -18,3 +17,10 @@ def create_access_token(data: dict) -> str:
     expire = datetime.now(timezone.utc) +  timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+def decode_access_token(token: str) -> int:
+    payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    user_id = payload.get("sub")
+    if user_id is None:
+        raise ValueError("Token missing subject")
+    return int(user_id )
